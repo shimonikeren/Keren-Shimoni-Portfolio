b0cfd30bc3be348492a3611d8d1d3b1da931025c
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-15 22:35:02 -0700

    add projects, more style

--- js/main.js
+++ js/main.js
@@ -2,44 +2,6 @@ $(document).ready(function() {
 
   'use strict';
 
-  //======================SMOOTH SCROLL====================//
-  $(document).on("scroll", onScroll);
-
-  $('a[href^="#"]').on('click', function(event) {  //Finds all links with an hreflang attribute
-    event.preventDefault();
-    $(document).off("scroll");
-
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
-
-    $(this).addClass('active');
-
-    var target = this.hash,
-        menu = target;
-
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
-
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
-
   //===================NAVBAR SHOW/HIDE==========================//
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
